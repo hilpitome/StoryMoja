cdc6e33d2b687fb6d477275739cbb9765714b6fc
hilpitome <hilpitome@gmail.com>
2015-07-25 13:41:41 +0300

    linked the ids to java

--- app/src/main/java/hilary/moringa/com/storymoja/MainActivity.java
+++ app/src/main/java/hilary/moringa/com/storymoja/MainActivity.java
@@ -4,14 +4,22 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    int counter;
+    Button add, sub;
+    TextView display;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        counter = 0;
+        add = (Button) (findViewById(R.id.addB));
+        sub = (Button) (findViewById(R.id.subB));
+        TextView = (Button) (findViewById(R.id.textView))
     }
 
     @Override
